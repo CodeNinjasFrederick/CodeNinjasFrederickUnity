1a047b952af66d768056194781ccfa972a904f3c
CodeNinjasFrederick <CodeNinjasFrederickSensei@outlook.com>
2022-07-26 18:57:59 -0400

    cool
    eliot test

---
+++ Eliot unity/ELP-Stacks/Assets/GameController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement
+
+public class GameController : MonoBehaviour
+{
+
+    [Header("Cube Object")]
+    public GameObject currentCube;
+    [Header("Last Cube Object")]
+    public GameObject lastCube;
+    [Header("Text object")]
+    public Text text;
+    [Header("Current Level")]
+    public int Level;
+    [Header("Boolean")]
+    public bool Done;
+
+    void NewBlock()
+    {
+        if(lastCube !- null)
+        {
+            currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x),
+                currentCube.transform.position.y,
+                Mathf.Round(currentCube.transform.position.z));
+            currentCube.transform.localScale = new Vector3(lastCube.trasform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x),
+                lastCube.transform.localScale.y,
+               lastCube.transform.localScale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
+            currentCube.transform.position = Vector3.Lerp(currentCube.transform.position, lastCube.transform.position, 0.5f) + Vector3.up * 5f;
+            if (currentCube.transform.localScale.x <= 0f ||
+                currentCube.transform.localScale.z <=0f)
+            {
+                Done = true;
+                text.gameObject.SetActive(true);
+                text.text = "Final Score: " + Level;
+                StartCorotine(X());
+                return;
+            }
+        }
+
+        lastCube = currentCube;
+        currentCube = Instantiate(lastCube);
+        currentCube.name = Level + "";
+        currentCube.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.HSVToRGB((Level / 100f) % 1f, 1f, 1f));
+        Level++;
+        Camera.main.transform.position = currentCube.transform.position + new Vector3(100, 100, 100);
+        Camera.main.transform.LookAt(currentCube.transform.position);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Done)
+        {
+            return;
+        }
+        var time = Mathf.Abs(Time.realtimeSinceStartup % 2f - 1f);
+        var pos1 = lastCube.transformation.position + Vector3.up * 10f;
+        var pos2 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
+    }
+}
