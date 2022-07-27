8f2a40c745f73922e22952a06fbbdbbc766c629a
CodeNinjasFrederick <CodeNinjasFrederickSensei@outlook.com>
2022-07-27 15:37:00 -0400

    Tommy added

---
+++ TB Games/sketch head/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    //Target object postion
+    [Header("Target Object")]
+    public Transform target;
+    // Update is called once per frame
+    void Update()
+    {
+        //If target position on the y axis is greater
+       //than the camera position
+       if(target.position.y > transform.position.y)
+
+        {
+            //The camera will follow the tergets postion 
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        
+          }
+    }
+}
---
+++ TB Games/sketch head/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControls : MonoBehaviour
+{
+    //Rigidbody2D object thatis stored
+    [Header("rigidbody")]
+    pulblic Rigidbody2D rb;
+   [Headder("Defalt Down Speed")]
+    //downward speed of the object
+    public float downSpeed = 20f;
+    [Header("Defalt Directional Movement Speed")]
+    //movement speed of the object 
+    public float movementSpeed = 10f;
+    [Header("Defalt Driectional Movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
