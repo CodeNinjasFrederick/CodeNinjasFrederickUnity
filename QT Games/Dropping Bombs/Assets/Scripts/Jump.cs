8928c0e145eaacd259b0753fac2a4f4d866ff0e6
CodeNinjasFrederick <CodeNinjasFrederickSensei@outlook.com>
2022-08-01 19:11:10 -0400

    Elliot Work

---
+++ QT Games/Dropping Bombs/Assets/Scripts/Jump.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    // strength of the push
+    public float jumpStrength = 10f;
+
+    private bool canJump;
+
+    private Rigidbody2D rigidBody;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rigidBody.velocity.y == 0)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+
+        if (canJump && Input.GetButton("Jump"))
+        {
+            // Apply an instantaneous upwards force
+            rigidBody.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
+            //canJump = !checkGround;
+        }
+
+    }
+
+}
---
+++ ZiUnitySaves/ZW-SuperFatShapes/Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    //Score is 0
+    public static int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Score is set to 0
+        //when the game starts
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Text UI component equals to 
+        //score as string
+        GetComponent<UnityEngine.UI.Text>().text = score.ToString();
+    }
+}
